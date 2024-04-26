3db7df17c2c09cc0bc9dbe4c4417e04ea30694d8
amitchell-lares <144437835+amitchell-lares@users.noreply.github.com>
2024-04-26 13:25:15 +0100

    eliminated global variable usage

--- allthewayback.py
+++ allthewayback.py
@@ -10,14 +10,8 @@ parser = argparse.ArgumentParser(
     prog="allthewayback", description="Search the WayBackMachine for senstive data"
 )
 
-# Default search from year, change with -y flag
-FROM_YEAR = "2020"
 # Wayback base URL
 WAYBACK_BASE_URL = "https://web.archive.org"
-# Wayback Rate Limit in seconds
-WAIT_TIME = 5
-# Check for written data during execution
-WRITE_CHECK = False
 
 
 # cmd arguments
@@ -50,12 +44,14 @@ def setup_arg_parser():
         dest="argFromYear",
         metavar="year",
         help="Year to start wayback searching from (Default: 2020)",
+        default="2020",
     )
     parsed_args.add_argument(
         "-rl",
         dest="argRateLimit",
         metavar="seconds",
         help="Rate Limit in seconds (Default: 5)",
+        default=5,
     )
     parsed_args.add_argument(
         "-v",
@@ -97,23 +93,25 @@ def setup_arg_parser():
 
 
 # Get list of entries from wayback machine based on search term
-def get_archives(target_host, search):
+def get_archives(target_host, search, from_year, wait_time):
     """
     Parses the host and search query and directs requests against API
 
     Arguments:
     target_host: target domain to be searched
     search: value to be searched for
+    from_year: year to start search from
+    wait_time: rate limit in seconds
     """
-    url = f"{WAYBACK_BASE_URL}/__wb/calendarcaptures/2?url={target_host}{search}&date={FROM_YEAR}"
+    url = f"{WAYBACK_BASE_URL}/__wb/calendarcaptures/2?url={target_host}{search}&date={from_year}"
     this_year = datetime.date.today().year
     combined_items = {"items": []}
-    print(f"[*] Getting list of {search} archives from {FROM_YEAR} onwards...")
+    print(f"[*] Getting list of {search} archives from {from_year} onwards...")
     print("[!] This will take about 5 seconds per year due to Wayback rate limits..")
-    for year in range(int(FROM_YEAR), this_year + 1):
+    for year in range(int(from_year), this_year + 1):
         try:
             response = requests.get(url, timeout=30)
-            time.sleep(WAIT_TIME)
+            time.sleep(wait_time)
             results = response.json()
             results.pop(
                 "colls", None
@@ -131,10 +129,10 @@ def get_archives(target_host, search):
         robot_archives = unpack_archives(combined_items)
         if robot_archives["items"]:
             print(
-                f"\n[+] Found {len(robot_archives['items'])} results dating back to {FROM_YEAR}"
+                f"\n[+] Found {len(robot_archives['items'])} results dating back to {from_year}"
             )
             url_list = [
-                f"{WAYBACK_BASE_URL}/web/{FROM_YEAR}0{archive[0]}/{target_host}{search}"
+                f"{WAYBACK_BASE_URL}/web/{from_year}0{archive[0]}/{target_host}{search}"
                 for archive in robot_archives["items"]
             ]
             return url_list
@@ -160,7 +158,7 @@ def unpack_archives(archives):
 
 def initalise(initial_args):
     """
-    Initializes and validates command-line arguments, sets global configurations.
+    Initializes and validates command-line arguments, sets configurations.
 
     Arguments:
     initial_args: Namespace - runtime arguments
@@ -168,15 +166,6 @@ def initalise(initial_args):
     Returns:
     str - The validated host domain to be used for archive searches.
     """
-    if initial_args.argFromYear:
-        global FROM_YEAR
-        FROM_YEAR = initial_args.argFromYear
-    if initial_args.argRateLimit:
-        global WAIT_TIME
-        WAIT_TIME = initial_args.argRateLimit
-    if initial_args.OUT_FILE:
-        global OUT_FILE
-        OUT_FILE = initial_args.OUT_FILE
     if "http" in initial_args.argDomain:
         print(
             "Please enter a domain without a protocol: google.com and not https://google.com"
@@ -187,29 +176,28 @@ def initalise(initial_args):
     return target_host
 
 
-def file_write(write_data, verb):
+def file_write(write_data, verb, out_file):
     """
-    Writes data to an output file specified by the OUT_FILE global variable. Data is appended if the file
+    Writes data to an output file specified by the runtime outfile variable. Data is appended if the file
     already exists, or a new file is created otherwise. Verbose output is shown if enabled.
 
     Arguments:
     writeData: list or str - The data to write. If a list, it will be joined by newlines.
     verb: bool - Toggle verbose output
+    out_file: str - Target file to write to
 
     Returns:
     str - The path to the output file where the data was written.
     """
-    mode = "a" if os.path.exists(OUT_FILE) else "w"
-    global WRITE_CHECK
-    WRITE_CHECK = True
-    with open(OUT_FILE, mode, encoding="utf-8") as f:
+    mode = "a" if os.path.exists(out_file) else "w"
+    with open(out_file, mode, encoding="utf-8") as f:
         if isinstance(write_data, list):
             f.write("\n".join(write_data) + "\n")
         else:
             f.write(write_data + "\n")
     if verb:
         print(f" {write_data} \n")
-    return OUT_FILE
+    return out_file
 
 
 def arg_worker(target_host, run_args):
@@ -224,66 +212,91 @@ def arg_worker(target_host, run_args):
     Returns:
     str or None - The filename where URLs were written, or None if no data was written.
     """
+    from_year = run_args.argFromYear
+    rate_limit = run_args.argRateLimit
+    out_file = run_args.OUT_FILE
+    verbose = run_args.verb
+    write_check = False
+
     if run_args.robots:
-        robot_urls = get_archives(target_host, "/robots.txt")
+        robot_urls = get_archives(target_host, "/robots.txt", from_year, rate_limit)
         if robot_urls:
-            target_filename = file_write(robot_urls, run_args.verb)
+            target_filename = file_write(robot_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No robots.txt data discovered")
+
     if run_args.git:
-        git_urls = get_archives(target_host, "/.git")
+        git_urls = get_archives(target_host, "/.git", from_year, rate_limit)
         if git_urls:
-            target_filename = file_write(git_urls, run_args.verb)
+            target_filename = file_write(git_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No .git data discovered")
+
     if run_args.config:
-        conf_urls = get_archives(target_host, "/config")
+        conf_urls = get_archives(target_host, "/config", from_year, rate_limit)
         if conf_urls:
-            target_filename = file_write(conf_urls, run_args.verb)
+            target_filename = file_write(conf_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No /config data discovered")
+
     if run_args.sitemap:
-        smap_urls = get_archives(target_host, "/sitemap.xml")
+        smap_urls = get_archives(target_host, "/sitemap.xml", from_year, rate_limit)
         if smap_urls:
-            target_filename = file_write(smap_urls, run_args.verb)
+            target_filename = file_write(smap_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No sitemap.xml data discovered")
+
     if run_args.htaccess:
-        htaccess_urls = get_archives(target_host, "/.htaccess")
+        htaccess_urls = get_archives(target_host, "/.htaccess", from_year, rate_limit)
         if htaccess_urls:
-            target_filename = file_write(htaccess_urls, run_args.verb)
+            target_filename = file_write(htaccess_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No .htaccess data discovered")
+
     if run_args.wconf:
-        wconf_urls = get_archives(target_host, "/web.config")
+        wconf_urls = get_archives(target_host, "/web.config", from_year, rate_limit)
         if wconf_urls:
             print(f" {wconf_urls} \n")
-            target_filename = file_write(wconf_urls, run_args.verb)
+            target_filename = file_write(wconf_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No web.config data discovered")
+
     if run_args.wxml:
-        wxml_urls = get_archives(target_host, "/WEB-INF/web.xml")
+        wxml_urls = get_archives(target_host, "/WEB-INF/web.xml", from_year, rate_limit)
         if wxml_urls:
-            target_filename = file_write(wxml_urls, run_args.verb)
+            target_filename = file_write(wxml_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No web.xml data discovered")
+
     if run_args.nginx:
-        nginx_urls = get_archives(target_host, "nginx.conf")
+        nginx_urls = get_archives(target_host, "nginx.conf", from_year, rate_limit)
         if nginx_urls:
-            target_filename = file_write(nginx_urls, run_args.verb)
+            target_filename = file_write(nginx_urls, verbose, out_file)
+            write_check = True
         else:
             print("[!] No nginx.conf data discovered")
+
     if run_args.ownFile:
         s_term = run_args.ownFile
-        own_urls = get_archives(target_host, s_term)
+        own_urls = get_archives(target_host, s_term, from_year, rate_limit)
         if own_urls:
-            target_filename = file_write(own_urls, run_args.verb)
+            target_filename = file_write(own_urls, verbose, out_file)
+            write_check = True
         else:
             print(f"[!] No {s_term} data discovered\n")
     else:
         pass
-    if not WRITE_CHECK:
-        target_filename = None
+
+    if not write_check:
+        return None
+
     return target_filename
 
 
@@ -297,8 +310,8 @@ if __name__ == "__main__":
     # Run the searches based on given arguments
     filename = arg_worker(host, args)
 
-    # Check if we've written to a file  and if we have, present the filename
-    if WRITE_CHECK:
+    # Check if we've written to a file and if we have, present the filename
+    if filename:
         print(f"[*] Saved results to {filename}")
     else:
         print("[!] Execution Finished: No data found :(")
